e603702d8d65cc9fb5f759e9e1f65f20a9499703
Karl Kivisild <karl.kivisild@voco.ee>
2023-09-19 12:02:52 +0300

    js variables - declare, assign and use

---
+++ variables.js
@@ -0,0 +1,21 @@
+// variables
+// keyword variablename = value;
+// let
+
+// muutuja tekitamine - declare
+let programmingLanguage = 'Java Script'
+console.log(programmingLanguage)
+
+// assignment - väärtuse omistamine - save new value into the variable
+programmingLanguage = 'PHP'
+console.log(programmingLanguage)
+
+
+let result
+console.log(result)
+
+// const
+
+const group = 'VS23'
+// group = 'PG23' - not allowed
+console.log(group)
