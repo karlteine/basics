ce0accb1a8714223fd5ba33c47c9dd4b5c8eba6e
Karl Kivisild <karl.kivisild@voco.ee>
2023-09-19 13:53:00 +0300

    data types and conversions and other stuff

---
+++ datatypes.js
@@ -0,0 +1,33 @@
+// data types
+// string
+let firstname = 'Karl'
+let lastname = 'Kivisild'
+
+console.log(firstname + " " + lastname)
+console.log(typeof firstname)
+
+// Numbers
+let goodmark = 5
+console.log(typeof goodmark)
+
+let percent = 50.3
+console.log(typeof percent)
+
+// Boolean
+let isToday = true
+console.log(typeof isToday)
+
+// type conversions
+// number --> String 
+console.log(typeof String(555))
+console.log(String(5+5))
+console.log(String(5.5)+ String(5.5))
+
+// boolean -> String
+console.log(String(true))
+
+// String -> Number
+console.log(Number('666'))
+console.log(Number('66,6'))
+console.log(parseInt(100.8))
+console.log(parseFloat(100.3))
