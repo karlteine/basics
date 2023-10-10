3a8ddeeb3953f0644d52603847d0c29009509296
Karl Kivisild <karl.kivisild@voco.ee>
2023-10-10 11:34:14 +0300

    Created project basic files

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const fs = require('fs');
+
+// use ejs files to prepare templates for views
+const path = require('path')
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+	// get data from file
+	fs.readFile('./views/tasks','utf8', (err, data) => {
+		if (err) {
+			console.log(err);
+			return;
+		}
+	// task list data from file
+	const tasks = data.split("\n")
+	res.render('index', {tasks: tasks})
+	});
+})
+
+
+app.listen(3002, () => {
+	console.log('Example app is started at http://localhost:3002')
+})
