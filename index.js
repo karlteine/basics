a13485d81ef594eee0961018cea862b3cc652f1b
Karl Kivisild <karl.kivisild@voco.ee>
2023-10-17 14:37:00 +0300

    Write and read data to file, delete single and all task data from file, add error handling

--- index.js
+++ index.js
@@ -7,18 +7,131 @@ const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+const readFile = (filename) => {
+  return new Promise((resolve, reject) => {
+    // get data from file
+    fs.readFile(filename, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+        return;
+      }
+      // task list data from file
+      const tasks = JSON.parse(data)
+      resolve(tasks)
+    });
+  });
+}	
+
+const writeFile = (filename, data) => {
+	return new Promise((resolve, reject) => {
+		// get data from file
+		fs.writeFile(filename, data, 'utf-8', err => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			resolve(true)
+		});
+	})
+}
+
 app.get('/', (req, res) => {
-	// get data from file
-	fs.readFile('./views/tasks','utf8', (err, data) => {
-		if (err) {
-			console.log(err);
-			return;
-		}
-	// task list data from file
-	const tasks = data.split("\n")
-	res.render('index', {tasks: tasks})
-	});
+  // tasks list data from file
+  readFile('./views/tasks.json')
+    .then(tasks => {
+      res.render('index', { 
+      	tasks: tasks,
+      	error: null
+    	})
+    })
+});
+
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/', (req, res) => {
+  // Control data from the form
+  let error = null;
+  if (req.body.task.trim().length === 0) {
+    error = 'Please insert correct task data';
+    return readFile('./views/tasks.json')
+      .then((tasks) => {
+        res.render('index', {
+          tasks: tasks,
+          error: error,
+        });
+      });
+  }
+
+  // Tasks list data from the file
+  readFile('./views/tasks.json')
+    .then((tasks) => {
+      // Add a new task
+      // Create a new ID automatically
+      let index;
+      if (tasks.length === 0) {
+        index = 0;
+      } else {
+        index = tasks[tasks.length - 1].id + 1;
+      }
+
+      // Create a task object
+      const newTask = {
+        "id": index,
+        "task": req.body.task
+      };
+
+      // Add the form-sent task to the task array
+      tasks.push(newTask);
+      data = JSON.stringify(tasks, null, 2);
+      return writeFile('./views/tasks.json', data);
+    })
+    .then(() => {
+      // Redirect to / to see the result
+      res.redirect('/');
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    });
+});
+
+app.get('/delete-task/:taskId', (req,res) => {
+	let deletedTaskId = parseInt(req.params.taskId)
+	readFile('./views/tasks.json')
+	.then(tasks => {
+		tasks.forEach((task, index) => {
+			if(task.id == deletedTaskId){
+				tasks.splice(index, 1)
+			}
+		})
+		data = JSON.stringify(tasks, null, 2)
+		writeFile('./views/tasks.json', data)
+		// redirect to / to see result
+		res.redirect('/')
+	})
 })
+
+app.get('/delete-tasks', (req, res) => {
+  // Read the existing tasks from the file
+  readFile('./views/tasks.json')
+    .then(tasks => {
+      // Clear the tasks array
+      tasks = [];
+      data = JSON.stringify(tasks, null, 2);
+      // Save the empty array back to the file
+      return writeFile('./views/tasks.json', data);
+    })
+    .then(() => {
+     // Redirect to / to see result
+     res.redirect('/');
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    });
+});
 
 
 app.listen(3002, () => {
